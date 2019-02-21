3ca1f774ddac879e2a03fac9835cb2f5463e4669
Ryan Hild <rhild@starbucks.com>
2019-02-20 22:51:07 -0800

    Use different variable for token verification

--- webapp/app/slackclient.py
+++ webapp/app/slackclient.py
@@ -4,7 +4,7 @@ from slackclient import SlackClient
 
 class SlackAPI:
     def __init__(self):
-        self.slack_token = os.environ.get('SLACK_APP_TOKEN')
+        self.slack_token = os.environ.get('SLACK_VERIFICATION_TOKEN')
         self.slack_client = SlackClient(self.slack_token)
 
     def process_event(self, form):
