38c936fa0814ca8a32a23524ef92e06a1d6ca3a1
Ryan Hild <rhild@starbucks.com>
2019-02-20 21:18:46 -0800

    Restructure & add slack methods

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask
+from app.utils.slack import SlackAPI
 
-app = Flask(__name__)
+app = Flask('slackbot')
 
 
 @app.route('/')
 def hello():
     return 'Hello world!'
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
