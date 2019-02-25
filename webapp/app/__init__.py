4458ea09265f63db6cc773c5c21c2391c20eb4ae
Ryan Hild <rhild@starbucks.com>
2019-02-24 20:06:33 -0800

    Recognize a couple of famous people

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -1,13 +1,15 @@
-from flask import Flask, Response, request
+from flask import Flask
 from slackeventsapi import SlackEventAdapter
 from slackclient import SlackClient
 from app.config import configure_app
-from app.twodegrees import two_degrees
-import json
+from app.twodegrees import TwoDegrees
+import queue
+import threading
 
 
 def create_app(custom_config=None):
     app = Flask('slackbot')
+    worker = WorkerThread()
 
     configure_app(app)
 
@@ -15,6 +17,7 @@ def create_app(custom_config=None):
         app.config.from_object(custom_config)
 
     bot_api = SlackClient(app.config.get('SLACK_BOT_TOKEN'))
+    two_degrees = TwoDegrees(bot_api)
 
     @app.route('/', methods=['GET'])
     def hello():
@@ -23,13 +26,32 @@ def create_app(custom_config=None):
     slack_events_adapter = SlackEventAdapter(app.config.get('SLACK_SIGNING_SECRET'), server=app)
 
     @slack_events_adapter.on('message')
-    async def message_channels(event_data):
-        print("INFO: raw message: " + str(event_data))
-        two_degrees(bot_api)(event_data)
+    def message_channels(event_data):
+        worker.enqueue(event_data, two_degrees.process)
 
     @slack_events_adapter.on('error')
     def error_handler(err):
         app.logger.error('slack api error: ' + str(err))
 
     print("Starting slackbot app")
+    worker.start()
     return app
+
+
+class WorkerThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.setDaemon(True)
+        self.queue = queue.Queue()
+
+    def run(self):
+        while True:
+            (event_data, processor) = self.queue.get()
+            try:
+                processor(event_data)
+            except Exception as err:
+                print("Worker Exception caught:", err)
+            self.queue.task_done()
+
+    def enqueue(self, event_data, processor):
+        self.queue.put((event_data, processor))
--- webapp/app/twodegrees.py
+++ webapp/app/twodegrees.py
@@ -1,12 +1,33 @@
-from flask import current_app
+class TwoDegrees:
+    def __init__(self, slack):
+        self.slack = slack
+        self.famous_people = [
+            "paris hilton",
+            "steve buscemi",
+        ]
+        self.places = [
+            "on the ferry",
+            "at the pub",
+            "while touring Europe"
+        ]
 
-
-def two_degrees(slack):
-    def process(event_data):
-        current_app.logger.error("message processing!")
+    def process(self, event_data):
         message = event_data["event"]
-        if message.get("subtype") is None and "hi" in message.get("text"):
+        if message.get("subtype") is None:
+            text = message.get("text").lower()
+            person = self.get_famous_person(text)
+            if person is None:
+                return
+            place = self.get_location()
             channel = message["channel"]
-            message = "Hello <@%s>! :tada:" % message["user"]
-            slack.api_call("chat.postMessage", channel=channel, text=message)
-    return process
+            message = "I once met %s at %s" % person, place
+            self.slack.api_call("chat.postMessage", channel=channel, text=message)
+
+    def get_famous_person(self, text):
+        for person in self.famous_people:
+            if person in text:
+                return person
+        return None
+
+    def get_location(self):
+        return self.places[2]
